c204dd00959ba4e9449691832fac4d71f18616c7
Alessandragr <alessandra-ribeiro@live.com>
2021-09-14 14:37:47 -0300

    Atividade filme, série e documentário

---
+++ src/modulo1/aula4/a4_3_switchCase.java
@@ -0,0 +1,25 @@
+package modulo1.aula4;
+
+public class a4_3_switchCase {
+    public static void main(String[] args) {
+        /*sintaxe:
+        switch (variavel) {
+        case 1:
+            System.out.printfl("Valor 1")
+            break;
+         case 2:
+          System.out.printfl("Valor 2")
+            break;
+          case 3:
+          System.out.printfl("Valor 3")
+            break;
+          default:
+          System.out.printfl("Valor 4")
+            break;
+            }
+
+         Só aceita:
+         */
+
+    }
+}
